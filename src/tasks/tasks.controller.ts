f14bb38a607d59dd934bb6d89486527a6c313509
dev-khalid <khalid.hossain.akash@gmail.com>
2021-09-08 20:53:15 +0600

    create read deleteing functionality done

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
+import { Request } from '@nestjs/common';
 
-@Controller('tasks')
+@Controller('/tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string) {
+    return this.tasksService.deleteTaskById(id);
+  }
+  
+  @Post()
+  createPost(
+    @Body() createTaskDto: CreateTaskDto,
+    // @Body() body, //this is the way to get all the body data in json formate.
+    // @Body('title') title: string,
+    // @Body('description') description: string,
+  ): Task {
+    // console.log('Body data: ', body);
+    return this.tasksService.createTask(createTaskDto);
   }
 }
